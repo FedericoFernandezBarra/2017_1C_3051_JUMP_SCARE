4576ce28766458b833680f4a27db59ea86726fa6
FedericoFernandezBarra <fede.a25@gmail.com>
2017-04-06 22:25:34 -0300

    Entrega #1, objetos 50%, mapa finalizado

---
+++ 2017_1C_3051_JUMP_SCARE/Model/GameModel.cs
@@ -0,0 +1,112 @@
+using Microsoft.DirectX;
+using Microsoft.DirectX.DirectInput;
+using System.Drawing;
+using TGC.Core.Direct3D;
+using TGC.Core.Example;
+using TGC.Core.Geometry;
+using TGC.Core.Input;
+using TGC.Core.SceneLoader;
+using TGC.Core.Textures;
+using TGC.Core.Utils;
+using TGC.Examples.Camara;
+
+namespace TGC.Group.Model
+{
+    /// <summary>
+    ///     Ejemplo para implementar el TP.
+    ///     Inicialmente puede ser renombrado o copiado para hacer más ejemplos chicos, en el caso de copiar para que se
+    ///     ejecute el nuevo ejemplo deben cambiar el modelo que instancia GameForm <see cref="Form.GameForm.InitGraphics()" />
+    ///     line 97.
+    /// </summary>
+    public class GameModel : TgcExample
+    {
+        /// <summary>
+        ///     Constructor del juego.
+        /// </summary>
+        /// <param name="mediaDir">Ruta donde esta la carpeta con los assets</param>
+        /// <param name="shadersDir">Ruta donde esta la carpeta con los shaders</param>
+        public GameModel(string mediaDir, string shadersDir) : base(mediaDir, shadersDir)
+        {
+            Category = Game.Default.Category;
+            Name = Game.Default.Name;
+            Description = Game.Default.Description;
+        }
+
+
+        //SCENE
+        private TgcScene Scene { get; set; }
+
+        //CAMARA
+        private TgcFpsCamera Camera{ get; set; }
+
+        /// <summary>
+        ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo.
+        ///     Escribir aquí todo el código de inicialización: cargar modelos, texturas, estructuras de optimización, todo
+        ///     procesamiento que podemos pre calcular para nuestro juego.
+        ///     Borrar el codigo ejemplo no utilizado.
+        /// </summary>
+        public override void Init()
+        {
+            //Device de DirectX para crear primitivas.
+            var d3dDevice = D3DDevice.Instance.Device;
+
+            TgcSceneLoader sceneLoader = new TgcSceneLoader();
+            Scene = sceneLoader.loadSceneFromFile(this.MediaDir + "Mapa\\MapaReal-TgcScene.xml");
+            Camara = new TgcFpsCamera(new Vector3(5962, 380, 1098), Input);
+
+
+        }
+
+        /// <summary>
+        ///     Se llama en cada frame.
+        ///     Se debe escribir toda la lógica de computo del modelo, así como también verificar entradas del usuario y reacciones
+        ///     ante ellas.
+        /// </summary>
+        public override void Update()
+        {
+            PreUpdate();
+ 
+
+        }
+
+        /// <summary>
+        ///     Se llama cada vez que hay que refrescar la pantalla.
+        ///     Escribir aquí todo el código referido al renderizado.
+        ///     Borrar todo lo que no haga falta.
+        /// </summary>
+        public override void Render()
+        {
+            //Inicio el render de la escena, para ejemplos simples. Cuando tenemos postprocesado o shaders es mejor realizar las operaciones según nuestra conveniencia.
+            PreRender();
+
+            Scene.renderAll();
+
+            //Dibuja un texto por pantalla
+            DrawText.drawText("2017_1C_3051_JUMP_SCARE (Federico Fernández Barra)", 0, 20, Color.Lime);
+            DrawText.drawText(
+                "Nota: Mapa realizado al 100%, faltan objetos (50% realizado)" , 0, 32,
+                Color.OrangeRed);
+            DrawText.drawText(
+                "Posición de la camara\n" + Camara.Position, 0, 45,
+                Color.Yellow);
+            DrawText.drawText(
+                "LookAt\n" + Camara.LookAt, 0, 110,
+                Color.LightBlue);
+
+
+
+            //Finaliza el render y presenta en pantalla, al igual que el preRender se debe para casos puntuales es mejor utilizar a mano las operaciones de EndScene y PresentScene
+            PostRender();
+        }
+
+        /// <summary>
+        ///     Se llama cuando termina la ejecución del ejemplo.
+        ///     Hacer Dispose() de todos los objetos creados.
+        ///     Es muy importante liberar los recursos, sobretodo los gráficos ya que quedan bloqueados en el device de video.
+        /// </summary>
+        public override void Dispose()
+        {
+            Scene.disposeAll();
+        }
+    }
+}
